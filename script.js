e9a415af7a85bc25779da77d5de2fc832c6f8f26
DavidLudington <diludington@gmail.com>
2024-03-27 11:50:05 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    console.log("Menu:", menu); 
+    console.log("Icon:", icon);
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
